1645f07f6f117ee2d6ad74d967770081006b91f4
maxim.sustaev <maxim.sustaev@yandex.ru>
2024-03-23 17:46:34 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -29,5 +29,26 @@ int countPairs2(int *arr, int len, int value) {
   return count;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+  int binarySearch (int arr[], int low, int high, int value) {
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+      if (arr[mid] == value) {
+        return mid;
+      } else if (arr[mid] < value) {
+          low = mid - 1;
+      } else {
+          high = mid - 1;
+      }
+    }
+    return 1;
+  }
+  int count = 0;
+  for (int i = 0; i < len; ++i) {
+    int secondElemnt = value - arr[i];
+    int index = binarySearch(arr, i + 1, len - 1, secondElement);
+    if (index != - 1) {
+      count++;
+    }
+  }
+  return count;
 }
