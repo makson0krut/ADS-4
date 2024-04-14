290a607bb4935ff257c8ce002e35de07d77212c4
maxim.sustaev <maxim.sustaev@yandex.ru>
2024-04-14 15:36:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,87 +1,68 @@
 // Copyright 2021 NNTU-CS
-using namespace std;
 
-int countPairs1(int *arr, int len, int value)
-{
-    int temp = 0, count = 0;
-    for (int i = 0; i < len; ++i) {
-        for (int j = 0; j < len - 1 - i; ++j) {
-            if (arr[j] > arr[j + 1]) {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-    for (int i = 0; i < len; ++i) {
-        for (int j = 1; j < len; ++j) {
-            if (arr[i] + arr[j] == value) {
-                count++;
-            }
-        }
-    }
-    return count;
-}
 
-int countPairs2(int *arr, int len, int value)
-{
-    int temp = 0, count = 0;
-    for (int i = 0; i < len; ++i) {
-        for (int j = 0; j < len - 1 - i; ++j) {
-            if (arr[j] > arr[j + 1]) {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-    for (int i = 0; i < len; ++i) {
-        for (int j = len; j >= len / 2 - 1; --j) {
-            if (arr[i] + arr[j] == value) {
-                count++;
-            }
-        }
-    }
-    return count;
-}
-
-int countPairs3(int *arr, int len, int value)
-{
-    int temp = 0;
-    for (int i = 0; i < len; ++i) {
-        for (int j = 0; j < len - 1 - i; ++j) {
-            if (arr[j] > arr[j + 1]) {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
+int countPairs1(int *arr, int size, int value) {
     int count = 0;
-    for (int i = 0; i < len - 1; ++i) {
-        int left = i, right = len - 1;
-        while (right > left) { 
-            int middle = (left + right) / 2; 
-            if (arr[i] + arr[middle] == value) {
+    for (int i = 0; i < size - 1; i++)
+        for (int j = i + 1; j < size; j++)
+            if ((arr[i] + arr[j]) == value)
+                count += 1;
+    return count;
+}
+
+int countPairs2(int* arr, int len, int value) {
+    int right = 0;
+    int count = 0;
+    int left = len - 1;
+    while (arr[right] > value) {
+        right -= 1;
+    }
+    for (int i = 0; i < right; i++) {
+        for (int j = right; j > i; j--) {
+            int sum = arr[i] + arr[j];
+            if (sum == value) {
                 count++;
-                int newmiddle = middle + 1; 
-                while (arr[i] + arr[newmiddle] == value && newmiddle < right) {
-                    count++;
-                    newmiddle++;
-                }
-                newmiddle = middle - 1;
-                while (arr[i] + arr[newmiddle] == value && newmiddle > left) {
-                    count++;
-                    newmiddle--;
-                }
-                break;
             }
-            if (arr[i] + arr[middle] > value) {
-                right = middle;
-            }
-            else
-                left = middle;
         }
+    }
+    return count;
+}
+
+int binsearch(int* arr, int size, int value) {
+    int right = size - 1;
+    int flag = 0;
+    int left = 0;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == value) {
+            flag++;
+            int left = mid - 1;
+            int right = mid + 1;
+            while (left >= 0 && arr[left] == value) {
+                flag++;
+                left--;
+            }
+            while (right < size && arr[right] == value) {
+                flag++;
+                right++;
+            }
+            return flag;
+        }
+        if (arr[mid] < value) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return flag;
+}
+
+int countPairs3(int* arr, int len, int value) {
+    int flag = 0;
+    for (int i = 0; i < len; i++) {
+        int second = value - arr[i];
+        flag += binsearch(&arr[i + 1], len - i - 1, second);
     }
     return count;
 }
